aed1f0969152da7b1e88db3f1ff7b46dc51f317e
Lomyal <wangshijun1995@gmail.com>
2014-11-16 23:19:57 +0800

    v0.0.1
    初始版本是错误的，缺了不少文件……这个才对……

---
+++ app.js
@@ -0,0 +1,69 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var partials = require('express-partials');
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
+var flash = require('connect-flash');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 9527);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(partials());
+  app.use(flash());
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: settings.cookieSecret,
+    store: new MongoStore({
+      db: settings.db
+    })
+  }));
+  // app.use(app.router);
+  //app.use(express.router(routes));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// app.get('/', routes.index);
+// app.get('/u/:user', routes.user);
+// app.post('/post', routes.post);
+// app.get('/reg', routes.reg);
+// app.post('/reg', routes.doReg);
+// app.get('/login', routes.login);
+// app.post('/login', routes.doLogin);
+// app.get('/logout', routes.logout);
+
+// express 3 之后给模板传入全局函数的方法 
+app.use(function(req, res, next) {
+  res.locals.user = req.session.user;
+
+  var err = req.flash('error');
+  var success = req.flash('success');
+
+  res.locals.error = err.length ? err : null;
+  res.locals.success = success.length ? success : null;
+
+  next();
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+routes(app);  // 作用同老写法： app.use(express.router(routes)); 
+
