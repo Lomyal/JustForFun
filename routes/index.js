93c34d6a3aab0f11b9bf6898b153ced94ad89fed
Lomyal <wangshijun1995@gmail.com>
2014-11-27 21:21:52 +0800

    v0.0.17
    Changes:
    1、首页又改回了简洁模式，格式与之前相比有微调，增加“了解更多”链接。
    2、所有project都用model这个词代替。
    3、model informatin 页面变为标签页，页面顶部格式与workspace保持一致。
    4、加入很原始的新建model的页面template_newmodel.ejs

--- routes/index.js
+++ routes/index.js
@@ -167,6 +167,13 @@ module.exports = function(app) {
       title: 'StigMod template!'
     });
   });
+  // 模板页get方法
+  app.get('/template_newmodel', checkLogin);
+  app.get('/template_newmodel', function(req, res){
+    res.render('template_newmodel', {
+      title: 'StigMod template!'
+    });
+  });
 
   // 临时跳转页get方法
   app.get('/tmpjmp', checkLogin);
