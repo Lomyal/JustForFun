1db9715a2f74726e3a380e5c90465c31ab6dbb97
Lomyal <wangshijun1995@gmail.com>
2015-01-16 15:38:08 +0800

    整理了js文件的结构和代码风格
    1、将stigmod.js拆分为workspace.js、modelview.js和debug.js。
    2、整理了workspace.js的代码风格。

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
     concat: {
       //dist: {
       //  src: 'build/src/*',
-      //  dest: 'dist/stigmod.js'
+      //  dest: 'dist/workspace.js'
       //}
     },
 
